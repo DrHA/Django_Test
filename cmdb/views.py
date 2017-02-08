e3aeea8db539421a626103579455433f4205b72f
DrHA <719520474@qq.COM>
2017-02-08 15:40:24 +0800

    第一版
    可以实现跳转，数据库

---
+++ cmdb/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from cmdb import models
+
+
+
+def index(request):
+    if request.method == 'POST':
+        uname = request.POST.get("uname",None)
+        pswd = request.POST.get("pswd",None)
+        is_sign = request.POST.get("is_sign",None)
+        print(is_sign)
+        if is_sign == "1":
+            return render(request,'sign.html')
+        print(uname,pswd)
+        models.UserInfo.objects.create(user=uname,pwd=pswd)
+        return render(request,'main.html')
+    # request.GET
+    # return HttpResponse("My First Django")
+    userlist = models.UserInfo.objects.all()
+    return render(request,"index.html", {"data": userlist})
+# Create your views here.
+
+def main(request):
+    return render(request,"main.html","")
+
+def sign(request):
+    return render(request,"sign.html","")
